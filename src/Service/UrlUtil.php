79620c09fa81ab4c2c8f42590525789260f783bb
Gregory COCHIN <Gregory.COCHIN@tech-advantage.com>
2019-12-10 18:43:37 +0100

    Add the Service UrlUtil to create edc url.

--- src/Service/UrlUtil.php
+++ src/Service/UrlUtil.php
@@ -3,6 +3,7 @@
 
 namespace Techad\EdcPopoverBundle\Service;
 
+use FlorianWolters\Component\Core\StringUtils;
 
 class UrlUtil
 {
@@ -12,10 +13,23 @@ class UrlUtil
      * @param string $subKey the secondary key
      * @param string $languageCode the language code
      * @param int $articleIndex the article index to display
-     * @return string the url for a context
+     * @return string the url for a edc context
      */
     public function getContextUrl(string $publicationId, string $mainKey, string $subKey, string $languageCode, int $articleIndex)
     {
         return "/context/" . $publicationId . "/" . $mainKey . "/" . $subKey . "/" . $languageCode . "/" . $articleIndex;
+    }
+
+    /**
+     * @param int $id the identifier of the document
+     * @param string $languageCode the language code of the document
+     * @param string $srcPublicationId the identifier of the publication
+     * @return string the url for a edc document
+     */
+    public function getDocumentationUrl(int  $id, string $languageCode = "", string $srcPublicationId = "")
+    {
+        $langCode = StringUtils::isNotBlank($languageCode) ? "/" . $languageCode : "";
+        $publicationId = StringUtils::isNotBlank($srcPublicationId) ? $srcPublicationId . "/" : "";
+        return "/doc/" . $publicationId . $id . $langCode;
     }
 }
--- tests/Service/UrlUtilTest.php
+++ tests/Service/UrlUtilTest.php
@@ -5,10 +5,34 @@ use Techad\EdcPopoverBundle\Service\UrlUtil;
 
 class UrlUtilTest extends TestCase
 {
-    public function test()
+    public function testShouldGetContextUrl()
     {
-        $urlUtl = new UrlUtil();
-        $url = $urlUtl->getContextUrl("pId", "mk", "sk", "en", 0);
+        $urlUtil = new UrlUtil();
+        $url = $urlUtil->getContextUrl("pId", "mk", "sk", "en", 0);
         $this->assertEquals("/context/pId/mk/sk/en/0", $url);
+    }
+
+    public function testShouldGetDocumentationUrlForId() {
+        $urlUtil = new UrlUtil();
+        $url = $urlUtil->getDocumentationUrl(1);
+        $this->assertEquals("/doc/1", $url);
+    }
+
+    public function testShouldGetDocumentationUrlForIdAndLanguageCode() {
+        $urlUtil = new UrlUtil();
+        $url = $urlUtil->getDocumentationUrl(1, "en");
+        $this->assertEquals("/doc/1/en", $url);
+    }
+
+    public function testShouldGetDocumentationUrlForIdAndPublication() {
+        $urlUtil = new UrlUtil();
+        $url = $urlUtil->getDocumentationUrl(1, "", "pId");
+        $this->assertEquals("/doc/pId/1", $url);
+    }
+
+    public function testShouldGetDocumentationUrlForIdAndLanguageCodedAndPublication() {
+        $urlUtil = new UrlUtil();
+        $url = $urlUtil->getDocumentationUrl(1, "en", "pId");
+        $this->assertEquals("/doc/pId/1/en", $url);
     }
 }
